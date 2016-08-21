40253952fba22779b09a72bf09f47cb255379fc6
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-08-21 22:07:23 +0200

    Deprecate EntityToPerson and PersonToEntity

--- openfisca_core/variables.py
+++ openfisca_core/variables.py
@@ -1,11 +1,12 @@
 import inspect
 import textwrap
 
-from openfisca_core.formulas import SimpleFormula, DatedFormula, EntityToPerson, PersonToEntity, new_filled_column
-from openfisca_core import columns
+from openfisca_core.formulas import SimpleFormula, DatedFormula, new_filled_column
 
 
 class AbstractVariable(object):
+    formula_class = None
+
     def __init__(self, name, attributes, variable_class):
         self.name = name
         self.attributes = {attr_name.strip('_'): attr_value for (attr_name, attr_value) in attributes.iteritems()}
@@ -26,10 +27,6 @@ class AbstractVariable(object):
             source_code, line_number = None, None
 
         return (comments, source_file_path, source_code, line_number)
-
-
-class AbstractComputationVariable(AbstractVariable):
-    formula_class = SimpleFormula
 
     def to_column(self, tax_benefit_system):
         formula_class = self.__class__.formula_class
@@ -72,139 +69,9 @@ class AbstractComputationVariable(AbstractVariable):
             )
 
 
-class Variable(AbstractComputationVariable):
+class Variable(AbstractVariable):
     formula_class = SimpleFormula
 
 
-class DatedVariable(AbstractComputationVariable):
+class DatedVariable(AbstractVariable):
     formula_class = DatedFormula
-
-
-class AbstractConversionVariable(AbstractVariable):
-    formula_class = None
-
-    def to_column(self, tax_benefit_system):
-
-        formula_class = self.__class__.formula_class
-
-        # Extract attributes.
-
-        cerfa_field = self.attributes.pop('cerfa_field', None)
-        if cerfa_field is not None:
-            assert isinstance(cerfa_field, basestring), cerfa_field
-            cerfa_field = unicode(cerfa_field)
-
-        doc = self.attributes.pop('doc', None)
-
-        entity_class = self.attributes.pop('entity_class')
-
-        label = self.attributes.pop('label', None)
-        label = self.name if label is None else unicode(label)
-
-        law_reference = self.attributes.pop('law_reference', None)
-        if law_reference is not None:
-            assert isinstance(law_reference, (basestring, list))
-
-        url = self.attributes.pop('url', None)
-        if url is not None:
-            url = unicode(url)
-
-        reference_variable = self.attributes.pop('variable')
-        reference_variable_name = unicode(reference_variable.__name__)
-        reference_column = tax_benefit_system.get_column(reference_variable_name)
-
-        if reference_column is None:
-            reference_column = tax_benefit_system.add_variable(reference_variable)
-            tax_benefit_system.automatically_loaded_variable.add(reference_variable_name)
-
-        # Build formula class and column from extracted attributes.
-
-        formula_class_attributes = dict(
-            __module__ = self.attributes.pop('module'),
-            )
-        if doc is not None:
-            formula_class_attributes['__doc__'] = doc
-
-        (comments, source_file_path, source_code, line_number) = self.introspect()
-
-        if comments is not None:
-            if isinstance(comments, str):
-                comments = comments.decode('utf-8')
-            formula_class_attributes['comments'] = comments
-        if source_file_path is not None:
-            formula_class_attributes['source_file_path'] = source_file_path
-        if source_code is not None:
-            formula_class_attributes['source_code'] = source_code
-        if line_number is not None:
-            formula_class_attributes['line_number'] = line_number
-
-        role = self.attributes.pop('role', None)
-        roles = self.attributes.pop('roles', None)
-        if role is None:
-            if roles is not None:
-                assert isinstance(roles, (list, tuple)) and all(isinstance(role, int) for role in roles)
-        else:
-            assert isinstance(role, int)
-            assert roles is None
-            roles = [role]
-        if roles is not None:
-            formula_class_attributes['roles'] = roles
-
-        formula_class_attributes['variable_name'] = reference_column.name
-
-        if issubclass(formula_class, EntityToPerson):
-            assert entity_class.is_person
-            column = reference_column.empty_clone()
-        else:
-            assert issubclass(formula_class, PersonToEntity)
-
-            assert not entity_class.is_person
-
-            if roles is None or len(roles) > 1:
-                operation = self.attributes.pop('operation')
-                assert operation in ('add', 'or'), 'Invalid operation: {}'.format(operation)
-                formula_class_attributes['operation'] = operation
-
-                if operation == 'add':
-                    if reference_column.__class__ is columns.BoolCol:
-                        column = columns.IntCol()
-                    else:
-                        column = reference_column.empty_clone()
-                else:
-                    assert operation == 'or'
-                    column = reference_column.empty_clone()
-            else:
-                column = reference_column.empty_clone()
-
-        # Ensure that all attributes defined in ConversionColumn class are used.
-        assert not self.attributes, 'Unexpected attributes in definition of filled column {}: {}'.format(self.name,
-            ', '.join(self.attributes.iterkeys()))
-
-        formula_class = type(self.name.encode('utf-8'), (formula_class,), formula_class_attributes)
-
-        # Fill column attributes.
-        if cerfa_field is not None:
-            column.cerfa_field = cerfa_field
-        if reference_column.end is not None:
-            column.end = reference_column.end
-        column.entity_class = entity_class
-        column.formula_class = formula_class
-        if reference_column.is_permanent:
-            column.is_permanent = True
-        column.label = label
-        column.law_reference = law_reference
-        column.name = self.name
-        if reference_column.start is not None:
-            column.start = reference_column.start
-        if url is not None:
-            column.url = url
-
-        return column
-
-
-class EntityToPersonColumn(AbstractConversionVariable):
-    formula_class = EntityToPerson
-
-
-class PersonToEntityColumn(AbstractConversionVariable):
-    formula_class = PersonToEntity

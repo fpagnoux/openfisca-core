f08e2a33abc53f49939004a7ad9249776098df8a
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-08-23 23:48:22 +0200

    Handle subroles in entities

--- openfisca_core/entities.py
+++ openfisca_core/entities.py
@@ -14,15 +14,22 @@ class Entity(object):
     def iter_member_persons_role_and_id(self, member):
         # one by one, yield individu_position, individu_role, individu_id
         index_in_entity = 0
-        for role in self.roles:
-            individus = member[role]
+        for role_name, role in self.roles.iteritems():
+            individus = member[role_name]
             if individus is not None:
                 if type(individus) == str:
                     individus = [individus]
 
-                for individu in individus:
-                    yield index_in_entity, role, individu
-                    index_in_entity += 1
+                if role.get('subroles'):
+                    subroles = role.get('subroles')
+                    index_in_subrole = 0
+                    for individu in individus:
+                        yield index_in_entity, subroles[index_in_subrole], individu
+                        index_in_subrole += 1
+                else:
+                    for individu in individus:
+                        yield index_in_entity, role_name, individu
+                        index_in_entity += 1
 
     def get_role_enum(self):
         return Enum(self.roles.keys())

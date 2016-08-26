d3dc91b712feb6889a09edfd60de55c9b7bd2aa1
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-08-26 19:44:54 +0200

    Refactor subroles

--- openfisca_core/entities.py
+++ openfisca_core/entities.py
@@ -8,31 +8,32 @@ class Entity(object):
     def __init__(self, key, label = u"", roles = {}, is_person = False):
         self.key = key
         self.label = label
-        self.roles = roles
         self.is_person = is_person
+        self.roles = roles
 
     def iter_member_persons_role_and_id(self, member):
-        # one by one, yield individu_position, individu_role, individu_id
+        # One by one, yield individu_position, individu_role, individu_id
         index_in_entity = 0
         role_index = 0
+        role_in_scenario_indexes = {}
+
         for role in self.roles:
-            role_name = role['key']
-            individus = member[role_name]
-            if individus is not None:
+            if role.get('role_in_scenario'):
+                role_name = role['role_in_scenario']
+                index = role_in_scenario_indexes.get(role_name) or 0
+                role_in_scenario_indexes[role_name] = index + 1
+                individus = (len(member[role_name]) > index) and member[role_name][index]
+            else:
+                role_name = role['key']
+                individus = member[role_name]
+
+            if individus:
                 if type(individus) == str:
                     individus = [individus]
 
-                if role.get('subroles'):
-                    subroles = role.get('subroles')
-                    index_in_subrole = 0
-                    for individu in individus:
-                        yield index_in_entity, subroles[index_in_subrole], individu
-                        index_in_subrole += 1
-                        role_index += 1
-                else:
-                    for individu in individus:
-                        yield index_in_entity, role_index, individu
-                        index_in_entity += 1
+                for individu in individus:
+                    yield index_in_entity, role_index, individu
+                    index_in_entity += 1
             role_index += 1
 
     def get_role_enum(self):

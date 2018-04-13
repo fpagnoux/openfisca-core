ae9033f532bacb54e24b9f48323c218baa47245c
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-04-13 09:39:02 -0400

    Poc lazy eval

--- test.py
+++ test.py
@@ -6,8 +6,27 @@ class ParameterNode(object):
 class ParameterNodeAtInstant(object):
   def __init__(self, node, instant):
       for child_name, child in node.children.items():
-        child_at_instant = child.get_at_instant(instant)
-        setattr(self, child_name, child_at_instant)
+        setattr(self, child_name, PlaceHolder(child, instant))
+
+  def __getattribute__(self, name):
+    attribute = object.__getattribute__(self, name)
+
+    if not isinstance(attribute, PlaceHolder):
+      return attribute
+
+    attribute = attribute.resolve()
+    setattr(self, name, attribute)
+
+    return attribute
+
+class PlaceHolder(object):
+  def __init__(self, child, instant):
+    self.child = child
+    self.instant = instant
+
+  def resolve(self):
+    print('Resolving {} for intant {}'.format(self.child.name, self.instant))
+    return self.child.get_at_instant(self.instant)
 
 class Child(object):
   def __init__(self, name):
@@ -20,3 +39,5 @@ class Child(object):
 node = ParameterNode({'rsa': Child('rsa'), 'cmu': Child('cmu')})
 
 node_at_instant = ParameterNodeAtInstant(node, '2018')
+
+# assert node_at_instant.cmu == 'cmu:2018'

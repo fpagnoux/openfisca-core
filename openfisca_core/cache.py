31efcf10e6987cea18b26154e37bc1097aed7795
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-06-21 03:05:13 +0200

    Add deletion

--- openfisca_core/cache.py
+++ openfisca_core/cache.py
@@ -4,7 +4,7 @@ from typing import Optional, List, Dict
 from collections import defaultdict
 
 from openfisca_core.types import Array
-from openfisca_core.periods import Period
+from openfisca_core.periods import Period, ETERNITY_PERIOD
 from openfisca_core.data_storage import Storage, InMemoryStorage
 
 
@@ -17,10 +17,12 @@ class Cache:
         return self._storage_by_variable[variable].get(period)
 
     def put_in_cache(self, variable: str, period: Period, value: Array) -> None:
+        if period == ETERNITY_PERIOD:
+            self._storage_by_variable[variable].is_eternal = True
         self._storage_by_variable[variable].put(value, period)
 
     def delete_arrays(self, variable: str, period: Optional[Period] = None) -> None:
-        pass
+        self._storage_by_variable[variable].delete(period)
 
     def get_known_periods(self, variable: str) -> List[Period]:
         pass
--- tests/core/test_u_cache.py
+++ tests/core/test_u_cache.py
@@ -23,3 +23,11 @@ def test_retrieve(cache):
     cache.put_in_cache('toto', period, value_to_cache)
     value_from_cache = cache.get_cached_array('toto', period)
     assert_equal(value_to_cache, value_from_cache)
+
+
+def test_delete(cache):
+    value_to_cache = array([10, 20])
+    cache.put_in_cache('toto', period, value_to_cache)
+    cache.delete_arrays('toto', period)
+    value_from_cache = cache.get_cached_array('toto', period)
+    assert value_from_cache is None
